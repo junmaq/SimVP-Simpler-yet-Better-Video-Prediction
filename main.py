b72b5ed3492e05fa11f5be5653a845f16a112483
junaid-maqbool-pv <junaid.maqboo.pv@gmail.com>
2022-07-12 16:29:40 +0500

    specified

--- main.py
+++ main.py
@@ -27,13 +27,13 @@ def create_parser():
     parser.add_argument('--in_shape', default=[8, 3, 256, 128], type=int,
                         nargs='*') # [10, 1, 64, 64] for mmnist, [4, 2, 32, 32] for taxibj
     parser.add_argument('--hid_S', default=156, type=int)
-    parser.add_argument('--hid_T', default=640, type=int)
+    parser.add_argument('--hid_T', default=704, type=int)
     parser.add_argument('--N_S', default=6, type=int)
     parser.add_argument('--N_T', default=8, type=int)
     parser.add_argument('--groups', default=4, type=int)
 
     # Training parameters
-    parser.add_argument('--epochs', default=1001, type=int)
+    parser.add_argument('--epochs', default=251, type=int)
     parser.add_argument('--log_step', default=4, type=int)
     parser.add_argument('--lr', default=0.01, type=float, help='Learning rate')
     return parser
